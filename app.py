48562a802fba2f289db681619e0e017864d06b67
xxaumx <89238427+xxaumx@users.noreply.github.com>
2021-11-18 11:18:35 +0700

    Update app.py

--- app.py
+++ app.py
@@ -159,7 +159,26 @@ def event_handle(event):
             replyObj = TextSendMessage(text="แม้งโก้วว")
             line_bot_api.reply_message(rtoken, replyObj)
         else :
-            replyObj = TextSendMessage(text=msg)
+            headers = request.headers
+            json_headers = json.dumps({k:v for k, v in headers.items()})
+            '''
+            json_line = request.get_json(force=False,cache=False)
+            json_line = json.dumps(json_line)
+            decoded = json.loads(json_line)
+            crl= pycurl.Curl()
+            crl.setopt( crl.URL, "https://bots.dialogflow.com/line/k--jomf/webhook")
+            crl.setopt( crl.POST, 1)
+            crl.setopt( crl.BINARYTRANSFER, true)
+            crl.setopt( crl.POSTFIELDS, decoded)
+            crl.setopt( crl.HTTPHEADER, json_headers)
+            crl.setopt( crl.SSL_VERIFYHOST, 2)
+            crl.setopt( crl.SSL_VERIFYPEER, 1)
+            crl.setopt( crl.FOLLOWLOCATION, 1)
+            crl.setopt( crl.RETURNTRANSFER, 1)
+            crl.perform()
+            crl.close()
+            '''
+            replyObj = TextSendMessage(text=json_headers)
             line_bot_api.reply_message(rtoken, replyObj)
     elif msgType == "image":
         try:
