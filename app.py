084161279f1bc0e0e9010b3a5018f305b5d3f089
xxaumx <89238427+xxaumx@users.noreply.github.com>
2021-11-18 11:25:50 +0700

    Update app.py

--- app.py
+++ app.py
@@ -166,7 +166,7 @@ def event_handle(event):
             json_line = json.dumps(json_line)
             decoded = json.loads(json_line)
             crl= pycurl.Curl()
-            crl.setopt( crl.URL, "https://bots.dialogflow.com/line/k--jomf/webhook")
+            crl.setopt( crl.URL, "https://bots.dialogflow.com/line/aum-rksf/webhook")
             crl.setopt( crl.POST, 1)
             crl.setopt( crl.BINARYTRANSFER, true)
             crl.setopt( crl.POSTFIELDS, decoded)
