b6c6538f87d9b746766deb007ae5b30c0d6c7caf
xxaumx <89238427+xxaumx@users.noreply.github.com>
2021-11-25 11:30:17 +0700

    Update app.py

--- app.py
+++ app.py
@@ -147,6 +147,7 @@ def event_handle(event,json_line):
         line_bot_api.reply_message(rtoken, replyObj)
         return ''
 
+  
     if msgType == "text":
         msg = str(event["message"]["text"])
         if (msg == "สวัสดี") :
