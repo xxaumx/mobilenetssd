164477924d22f93384b994988a44bc30ea673373
xxaumx <89238427+xxaumx@users.noreply.github.com>
2021-12-22 21:34:55 +0700

    Update app.py

--- app.py
+++ app.py
@@ -125,7 +125,6 @@ def reply(intent,text,reply_token,id,disname):
     line_bot_api.reply_message(reply_token,text_message)
 
 def event_handle(event,json_line):
-    print(event)
     try:
         userId = event['source']['userId']
     except:
